8cbb7880b6130cca92f52eabe4c63155f677a15f
Maxi <maximiliano.arevalo@usach.cl>
2021-05-23 19:35:45 -0400

    working

--- Taller 2/lab2.r
+++ Taller 2/lab2.r
@@ -10,8 +10,8 @@ colnames(data) <- c("age","anaemia","creatinine_phosphokinase","diabetes","eject
                     "time","DEATH_EVENT")
 
 #Se deben pasar todas las variables a int
-data <- data[-c(1),]
-data$age <-as.numeric(data$age)
+#data <- data[-c(1),]
+data$age <-as.integer(data$age)
 data$anaemia <-as.numeric(data$anaemia)
 data$creatinine_phosphokinase <-as.numeric(data$creatinine_phosphokinase)
 data$diabetes <-as.numeric(data$diabetes)
@@ -30,7 +30,17 @@ data$DEATH_EVENT <-as.numeric(data$DEATH_EVENT)
 data3 <- data[,c(1,3,5,7,8,9,13)]
 data3$DEATH_EVENT <- factor(data3$DEATH_EVENT)
 
-set.seed(71)
+# Como se tiene un desequilibrio en el dataset, debido a que 203 pacientes sobreviven y 96 pacientes mueren
+# Por lo que existe una diferencia de 103 pacientes que sobreviven, se eliminarán los que tienen características más parecidas
+
+data3 <- data3[-c(1,228,22,90,260,175,148,194,89,78,208,225,277,125,245,273,
+                  45,273,45,38,55,179,91,66,83,103,137,198,100,162,251,274,
+                  108,243,157,114,226,35,147,239,276,144,272,141,58,244,295,163,
+                  143,217,65,250,290,220,109,123,274,246,297,206,262,208,
+                  72,162,194,170,59,240,263,181,282,207,300,254,108,93,99,
+                  177,204,129,132),]
+
+
 training.ids <- createDataPartition(data3$DEATH_EVENT, p = 0.7, list = FALSE)
 
 #Un tipo de randomForest
@@ -112,7 +122,3 @@ print(mod4) # Se obtiene un 23,99% de OOB
 #mtry = 3 / testeo
 mod5 <- randomForest(DEATH_EVENT ~ ., data = importantData, ntree = 500, mtry = 3, importance = TRUE, proximity = TRUE) 
 print(mod5) # Se obtiene un 23,99% de OOB -> disminuye
-
-#mtry = 6 / testeo
-mod6 <- randomForest(DEATH_EVENT ~ ., data = importantData, ntree = 500, mtry = 6, importance = TRUE, proximity = TRUE) 
-print(mod6) # Se obtiene un 23,99% de OOB
